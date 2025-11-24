e94f735ba1afefd59624364973c9221e652ea559
Ayushi V <ayushiverma0909@gmail.com>
2025-11-23 22:53:44 -0500

    backend

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,11 @@ const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
 app.use(cors({
     origin: FRONTEND_URL,
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 
 app.use(express.json());
 app.use("", routes);
